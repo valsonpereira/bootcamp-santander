b1ddf23762bb16aed1cdd41e8f46628b50d0bcdf
valsonpereira <valson.pereira@gmail.com>
2021-08-27 09:52:55 -0300

    create database and repository

---
+++ src/main/java/com/santander/bootcamp/repository/StockRepository.java
@@ -0,0 +1,9 @@
+package com.santander.bootcamp.repository;
+
+import com.santander.bootcamp.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Long> {
+}
